88b281894f167e56364f6137fe523d99a2423996
Aligazy Kismetov <99682480+Al1gazY@users.noreply.github.com>
2025-09-22 11:49:11 -0400

    avg step count calculations

---
+++ Step Tracker/Charts/ChartDataTypes.swift
@@ -0,0 +1,14 @@
+//
+//  ChartDataTypes.swift
+//  Step Tracker
+//
+//  Created by Aligazy Kismetov on 2025-09-22.
+//
+
+import Foundation
+
+struct WeekdayChartData: Identifiable {
+    let id = UUID()
+    let date: Date
+    let value: Double
+}
---
+++ Step Tracker/Extensions/Date+Ext.swift
@@ -0,0 +1,14 @@
+//
+//  Date+Ext.swift
+//  Step Tracker
+//
+//  Created by Aligazy Kismetov on 2025-09-22.
+//
+
+import Foundation
+
+extension Date {
+    var weekdayInt: Int {
+        Calendar.current.component(.weekday, from: self)
+    }
+}
